98badf5768526f7f4b1fca3bcaff00f2041cde0f
dsputra12 <dsputra1202@gmail.com>
2024-07-07 09:01:48 +0700

    setiap seluruh activity

--- app/src/main/java/com/sofeed/myapp/HomePage2.kt
+++ app/src/main/java/com/sofeed/myapp/HomePage2.kt
@@ -1,18 +1,27 @@
 package com.sofeed.myapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 import com.sofeed.myapp.databinding.FragmentHomePageBinding
 
 
 class HomePage2 : Fragment() {
-    private  lateinit var imageView : ImageView
+    private  lateinit var frame4 : FrameLayout
+    private lateinit var frame1: FrameLayout
+    private lateinit var  frame2: FrameLayout
+    private lateinit var frame3: FrameLayout
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
     private lateinit var binding: FragmentHomePageBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +37,33 @@ class HomePage2 : Fragment() {
         // Apply window insets
         enableEdgeToEdge(binding.root)
 
-        imageView = view.findViewById(R.id.cardFormulasi)
-        imageView.setOnClickListener{
+        frame4 = view.findViewById(R.id.containerFormulasi)
+        frame4.setOnClickListener{
             navigateToFormulasi()
+        }
+
+        frame1 = view.findViewById(R.id.containerInformasiPakan)
+        frame1.setOnClickListener{
+            navigateToInformasiPakan()
+        }
+
+        frame2 = view.findViewById(R.id.containerMetana)
+        frame2.setOnClickListener{
+            navigateToMetana()
+        }
+
+        frame3 = view.findViewById(R.id.containerMarket)
+        frame3.setOnClickListener{
+            navigateToMarket()
+        }
+
+        val firebaseUser = firebaseAuth.currentUser
+
+        if (firebaseUser != null) {
+            binding.namaUser.text = firebaseUser.displayName
+        } else {
+            startActivity(Intent(requireContext(), SignIn::class.java))
+            activity?.finish()
         }
     }
 
@@ -48,5 +81,29 @@ class HomePage2 : Fragment() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             WindowInsetsCompat.CONSUMED
         }
+    }
+
+    private fun navigateToInformasiPakan (){
+        val InformasiPakanFragment = BahanPakan()
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, InformasiPakanFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun navigateToMetana(){
+        val MetanaFragment = BahanPakan()
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, MetanaFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun navigateToMarket(){
+        val MarketFragment = HargaPakan()
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, MarketFragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
--- app/src/main/java/com/sofeed/myapp/LakukanFormulasi.kt
+++ app/src/main/java/com/sofeed/myapp/LakukanFormulasi.kt
@@ -1,17 +1,28 @@
 package com.sofeed.myapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class LakukanFormulasi : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var searchView: SearchView
+    private lateinit var button: Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,8 +33,27 @@ class LakukanFormulasi : AppCompatActivity() {
             WindowInsetsCompat.CONSUMED
         }
 
+        val items = listOf("Sapi Pedaging", "Sapi Perah")
+
+        val autoComplete : AutoCompleteTextView = findViewById(R.id.autoComplete)
+
+        val adapter = ArrayAdapter(this, R.layout.jenis_hewan, items)
+        autoComplete.setAdapter(adapter)
+
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener {
+                adapterView, view, i, l ->
+            val itemSelected = adapterView.getItemAtPosition(i)
+            Toast.makeText(this, "Item: $itemSelected", Toast.LENGTH_SHORT).show()
+        }
+
         recyclerView = findViewById(R.id.recyclerView)
         searchView = findViewById(R.id.searchView)
+        button = findViewById(R.id.buttonFormulasi)
+
+        button.setOnClickListener {
+            startActivity(Intent(this,HasilFormulasi::class.java))
+        }
+
 
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(this)
